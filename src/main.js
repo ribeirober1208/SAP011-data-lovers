906ee1d5cf3cf46029ea3612aeaf7d07e06a07ce
2karina <karina_kap@hotmail.com>
2023-08-11 19:33:14 -0300

    Aplicado os cards em main.js, não alterado CSS

--- src/main.js
+++ src/main.js
@@ -1,6 +1,36 @@
-import { example } from './data.js';
+/*import { example } from './sand-data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+console.log(example, data);*/
+
+
+fetch('./data/got/got.json')
+    .then(Response => Response.json())
+    .then(data => {
+        const cardsContainer = document.querySelector('.cards-container');
+
+        data.got.forEach(character => {
+            const card = document.createElement('div');
+            card.classList.add('card');
+
+            const image = document.createElement('img');
+            image.src = character.imageUrl;
+            image.alt = character.fullName;
+
+            const firstName = document.createElement('h2');
+            firstName.textContent = character.firstName;
+
+            const lastName = document.createElement('p');
+            lastName.textContent = character.lastName;
+
+            card.appendChild(image);
+            card.appendChild(firstName);
+            card.appendChild(lastName);
+            cardsContainer.appendChild(card);
+        });
+    })
+.catch(error => {
+    console.error('Error loading data:', error);
+});
