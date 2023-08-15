b0e6e944ffeacba9c2b4331f3e6de6369d3648de
2karina <karina_kap@hotmail.com>
2023-08-15 20:29:36 -0300

    Inserido filtros input e familia, corrigiido funcionalidade do filtro AZ/ZA

--- src/main.js
+++ src/main.js
@@ -10,34 +10,25 @@ fetch('./data/got/got.json')
     .then(response => response.json())
     .then(data => {
         const cardsContainer = document.getElementById('cardsContainer'); // Corrigido o acesso ao elemento
-
-        function createCard(character) {
+        data.got.forEach(character => {
             const card = document.createElement('div');
             card.classList.add('card', 'flippable');
-
             const cardInner = document.createElement('div');
             cardInner.classList.add('card-inner');
-
             const cardFront = document.createElement('div');
             cardFront.classList.add('front');
-
             const cardBack = document.createElement('div');
             cardBack.classList.add('back');
-
             const image = document.createElement('img');
             image.src = character.imageUrl;
             image.alt = character.fullName;
-
             const firstName = document.createElement('h2');
             firstName.textContent = character.firstName;
-
             const lastName = document.createElement('p');
             lastName.textContent = character.lastName;
-
             cardFront.appendChild(image);
             cardFront.appendChild(firstName);
             cardFront.appendChild(lastName);
-
             const characterInfo = document.createElement('div');
             characterInfo.classList.add('character-info');
             characterInfo.innerHTML = `
@@ -45,40 +36,12 @@ fetch('./data/got/got.json')
                 <p>Family: ${character.family}</p>
                 <p>Born: ${character.born}</p>
             `;
-
             cardBack.appendChild(characterInfo);
-
             cardInner.appendChild(cardFront);
             cardInner.appendChild(cardBack);
-
             card.appendChild(cardInner);
-
-            return card;
-        }
-
-        function renderCards(characters) {
-            cardsContainer.innerHTML = '';
-            characters.forEach(character => {
-                const card = createCard(character);
-                cardsContainer.appendChild(card);
-            });
-        }
-
-        renderCards(data.got);
-
-        const filterSelect = document.getElementById('filter');
-        filterSelect.addEventListener('change', () => {
-            const selectedValue = filterSelect.value;
-
-            if (selectedValue === 'a-z') {
-                data.got.sort((a, b) => a.fullName.localeCompare(b.fullName));
-            } else if (selectedValue === 'z-a') {
-                data.got.sort((a, b) => b.fullName.localeCompare(a.fullName));
-            }
-
-            renderCards(data.got);
+            cardsContainer.appendChild(card);
         });
-
         const cards = document.querySelectorAll('.card');
         cards.forEach(card => {
             card.addEventListener('click', () => {
